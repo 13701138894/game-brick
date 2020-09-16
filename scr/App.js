4372cb4c4d38705ef23a0dc55814e5d86eb9f7e7
13701138894 <13701138894@163.com>
2020-09-16 10:46:15 +0800

    验证commit检测

--- scr/App.js
+++ scr/App.js
@@ -1,6 +1,6 @@
 import { defineComponent,h } from "@vue/runtime-core";
 import GamePage from "./page/GamePage";
-
+console.log('123')
 export default defineComponent({
     render(){
         return h('Container',{},[
